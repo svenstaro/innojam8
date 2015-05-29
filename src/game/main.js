63c8cc86ad92a3763c4ef40f282388d159d7e66e
Sven-Hendrik Haase <sh@lutzhaase.com>
2015-05-29 22:02:18 +0200

    Tabs to spaces

--- src/game/main.js
+++ src/game/main.js
@@ -3,34 +3,34 @@ game.module(
 )
 .body(function() {
 
-	game.addAsset('logo.png');
+    game.addAsset('logo.png');
 
-	game.createScene('Main', {
-	    backgroundColor: 0xb9bec7,
+    game.createScene('Main', {
+        backgroundColor: 0xb9bec7,
 
-	    init: function() {
-	        var logo = new game.Sprite('logo.png').center().addTo(this.stage);
-	    },
+        init: function() {
+            var logo = new game.Sprite('logo.png').center().addTo(this.stage);
+        },
 
-	    keydown: function(key) {
-	        if (key === 'SPACE') {
-	            // Space key down
-	        }
-	    },
+        keydown: function(key) {
+            if (key === 'SPACE') {
+                // Space key down
+            }
+        },
 
-	    keyup: function(key) {
-	        if (key === 'SPACE') {
-	            // Space key up
-	        }
-	    },
+        keyup: function(key) {
+            if (key === 'SPACE') {
+                // Space key up
+            }
+        },
 
-	    update: function() {
-	        // Check if key is currently down
-	        if (game.keyboard.down('SPACE')) {
-	            // Space is down
-	        }
-	        this._super();
-	    }
-	});
+        update: function() {
+            // Check if key is currently down
+            if (game.keyboard.down('SPACE')) {
+                // Space is down
+            }
+            this._super();
+        }
+    });
 
 });
