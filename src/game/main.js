0a277a3bd804dc636996d5783d58d1e6187a2413
Sven-Hendrik Haase <sh@lutzhaase.com>
2015-05-29 23:46:59 +0200

    Add box2d plugin and remove our physics

--- src/game/main.js
+++ src/game/main.js
@@ -1,6 +1,7 @@
 game.module(
     'game.main'
 )
+.require('plugins.box2d')
 .require('game.game_over')
 .require('game.worldsphere')
 .require('game.player')
@@ -13,28 +14,6 @@ game.module(
         init: function() {
             this.worldsphere = new game.WorldSphere(100, 100, 50);
             this.player = new game.Player(500, 500);
-
-            // Create physics world
-            this.world = new game.World();
-
-            // Create new body
-            var body = new game.Body();
-
-            // Set body position
-            body.position.x = 200;
-            body.position.y = 200;
-
-            // Set body mass, so it will fall
-            body.mass = 1;
-
-            // Create new shape with size of 100px x 100px
-            var shape = new game.Rectangle(100, 100);
-
-            // Add shape to body
-            body.addShape(shape);
-
-            // Add body to world
-            this.world.addBody(body);
 
             this.entities = game.pool.create('MainPool');
             // game.pool.put('MainPool', meinobject);
