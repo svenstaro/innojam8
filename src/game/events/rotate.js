fcfd3975d8bc8e712e2badc61e688b7f6a2b60a6
Rafael Epplee <rafael.epplee@sinnerschrader.com>
2015-05-30 19:30:06 +0200

    Balance sphere rotation

--- src/game/events/rotate.js
+++ src/game/events/rotate.js
@@ -2,7 +2,7 @@ game.module('game.events.rotate')
 .body(function() {
     game.createClass('Rotate', {
         init: function(difficulty) {
-            var maxRotation = difficulty;
+            var maxRotation = 1 + difficulty / 10;
             game.scene.sphere.rotate(Math.random()*maxRotation - (maxRotation/2));
         }
     });
