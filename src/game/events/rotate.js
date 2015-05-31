7ff421827a741f7ea3ab065840a98556f24f66d3
Rafael Epplee <rafael.epplee@sinnerschrader.com>
2015-05-31 04:32:31 +0200

    Adjust rotation

--- src/game/events/rotate.js
+++ src/game/events/rotate.js
@@ -2,7 +2,7 @@ game.module('game.events.rotate')
 .body(function() {
     game.createClass('Rotate', {
         init: function(difficulty) {
-            var maxRotation = 1 + difficulty / 10;
+            var maxRotation = 3 + difficulty;
             game.scene.sphere.rotate(Math.random()*maxRotation - (maxRotation/2));
         }
     });
