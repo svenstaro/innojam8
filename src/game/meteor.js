61b351171c4dfb1ad6d70a453ddee6d18a679b2e
Rafael Epplee <rafael.epplee@sinnerschrader.com>
2015-05-31 12:25:24 +0200

    Adjust meteor destroy time

--- src/game/meteor.js
+++ src/game/meteor.js
@@ -42,7 +42,7 @@ game.module('game.meteor')
 
             this.body.SetUserData(this);
             this.destroy = false;
-            this.destroyTime = 10;
+            this.destroyTime = 500;
         },
 
         update: function() {
